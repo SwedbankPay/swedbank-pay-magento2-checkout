d15acd37d7bcabb3dda67156ce9f68b1f2104860
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2019-05-21 22:39:47 +0200

    Add documentation to the Config class
    Add documentation to the Config class to remove the PHPCS warning
    "Missing doc comment for class Config":
    
    https://travis-ci.org/PayEx/payex-magento2-checkout/jobs/535468403#L884

--- Helper/Config.php
+++ Helper/Config.php
@@ -4,6 +4,9 @@ namespace PayEx\Checkout\Helper;
 
 use PayEx\Core\Helper\Config as CoreConfig;
 
+/**
+ * Configuration class.
+ */
 class Config extends CoreConfig
 {
     const XML_CONFIG_GROUP = 'checkout';
