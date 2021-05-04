3f4506e7bd2d30f1023352c946c9be5039a848a9
Md Shahriar Siraj <shahriar@trollwebsolutions.se>
2021-05-04 16:20:04 +0200

    [FEATURE] Displays shipping methods only when checkin is done through the iframe

---
+++ view/frontend/web/js/action/shipping-methods-view.js
@@ -0,0 +1,13 @@
+define([
+    'jquery',
+    'ko',
+], function ($, _) {
+    'use strict';
+
+    return {
+        show: function(){
+            // Function needs to be hooked into
+            // In order to display the list of shipping methods
+        }
+    }
+});
